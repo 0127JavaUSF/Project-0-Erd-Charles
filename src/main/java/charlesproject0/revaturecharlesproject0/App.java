e55c9b6b2e177e2c1be346fb7a1cce6f19b6c464
charley43613 <41710701+charley43613@users.noreply.github.com>
2020-02-04 13:21:43 -0500

    first commit blank project

---
+++ src/main/java/charlesproject0/revaturecharlesproject0/App.java
@@ -0,0 +1,13 @@
+package charlesproject0.revaturecharlesproject0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
